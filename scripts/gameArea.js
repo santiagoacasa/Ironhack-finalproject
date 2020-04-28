094540d4ef97e95bfd32c85742c72806f6130ab4
santicasa <39978918+santiagoacasa@users.noreply.github.com>
2020-04-28 16:08:18 +0200

    JS and HTML ids
    Funciones para inicializar el canvas, cambiar aletatoriamente de color y para actualizar el nombre del jugador en el aside

--- scripts/gameArea.js
+++ scripts/gameArea.js
@@ -4,13 +4,16 @@ const context = canvas.getContext('2d');
 const player = {
     name: "",
     color: "",
-    arrRGB: [255,0,0],
+    red: 'rgb(255,0,0)',
+    green: 'rgb(0,255,0)',
+    blue: 'rgb(0,0,255)',
+    arrRGB: [],
     x: 0,
     y:0,
     width: 20,
     height: 20,
-    speedX: 6,
-    speedY: 6,
+    speedX: 12,
+    speedY: 12,
     lives: 3,
     score: 0,
     dibujar(){
@@ -18,9 +21,10 @@ const player = {
         context.fillRect(this.x, this.y, this.width,this.height);
     },
     iniciarlizar(){
-       this.color = `rgb(${this.arrRGB[0]},${player.arrRGB[1]},${player.arrRGB[2]})`;
+       this.color = this.red;
        this.x = 250;
        this.y = canvas.height - this.height;
+       this.arrRGB.push(this.red, this.green,this.blue);
        this.dibujar();
     },
     mover(event){
@@ -48,9 +52,40 @@ const player = {
             default:
                 break;
         };
-    }
+    },
+   
+}
+
+const obstaculos = {
+    color: "",
+    red: 'rgb(255,0,0)',
+    green: 'rgb(0,255,0)',
+    blue: 'rgb(0,0,255)',
+    arrRGB: [],
+    x: 0,
+    y:0,
+    width: 20,
+    height: 20,
+    speedX: 12,
+    speedY: 12,
+    crear(){
+
+    },
+    dibujar(){
+
+    },
 }
 
 const iniciarCanvas = function(){
     context.clearRect(0,0,canvas.width,canvas.height);
 };
+
+const randomColor = function(obj){
+    obj.color = obj.arrRGB[Math.floor(Math.random() * obj.arrRGB.length)];
+};
+
+const setPlayerName = function(){
+    let playerName = document.querySelector("#playerName");
+    let newPlayer = document.querySelector("#nombreUsuario span"); 
+    newPlayer.innerHTML = `Hola ${playerName.value}!`;
+}
--- scripts/juego.js
+++ scripts/juego.js
@@ -1,7 +1,13 @@
 window.onload = () => {
+    document.querySelector("#setName").addEventListener('click', setPlayerName);
+    const intervalCallback = function(){
+        randomColor(player);
+    };
+    //Desde aca iria en el boton "Jugar"
     iniciarCanvas();
     player.iniciarlizar();
     document.addEventListener('keydown', event => player.mover(event));
+    const myInterval = setInterval(intervalCallback, 1000);
     requestAnimationFrame(update);
 }
 
